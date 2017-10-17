5104d3cb9a3e0eb7ea4fd0af56a8228725bd344b
BChrabascz17 <32140897+BChrabascz17@users.noreply.github.com>
2017-10-17 14:13:16 -0400

    Add files via upload

--- 4-Arrays.js
+++ 4-Arrays.js
@@ -10,28 +10,37 @@
 // After each scoop alert say heres your flavor
 //after  loop, alert heres your sunday
 
-
-for (i = 1; i < 4; i++) { 
-    
-
-
+var ourFlavors = ["chocolate","vanilla","strawberry"];
+var scoops = [];
+for (scoop = 0; scoop < 3; scoop++) { 
 
 		var flavor = prompt("Please enter a flavor such as Chocolate, Vanilla or Strawberry");
-		if(flavor == "chocolate" || flavor == "vanilla"){
-			alert("The flavor you selected is "+flavor);
-			alert("Scoop #"+i+". Your flavor is "+flavor);
-		}
-		else if(flavor == "strawberry"){
-			alert("Sorry we do not have any "+flavor+" ice cream left.");
-			i--;
-			alert("Please select a different flavor");
+		if(inArray(flavor, ourFlavors)== true){
+			
+				if(flavor == "strawberry"){
+					alert("Sorry we do not have any "+flavor+" ice cream left.");
+					scoop--;
+					alert("Please select a different flavor");
+				}
+				else{
+					scoops[scoop] = flavor
+				}
 		}
 		else{
-			alert("Sorry. There is no such flavor in our store");
-			i--;
-			alert("Please select a different flavor");
+			alert("Sorry. There is no "+flavor+" flavor in our store");
+			scoop=scoop - 1;
+			
 		}
-	
-	
-	
+}
+
+alert("Here's your "+scoops+" cone");
+
+function inArray(svalue, tarray){
+	answer = false
+	for(index = 0; index < tarray.length; index++){
+		if(svalue == tarray[index]){
+			answer = true;
+		}
+	}
+	return answer;
 }
