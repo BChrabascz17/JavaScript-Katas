cfe8611b953a06fda28876322d1fd1de742aa5ff
BChrabascz17 <32140897+BChrabascz17@users.noreply.github.com>
2017-10-17 12:24:16 -0400

    Add files via upload
    not done

---
+++ 4-Arrays.js
@@ -0,0 +1,37 @@
+/*1.2 While Loop*/
+
+//Conditional where flavor == chocolate or vanilla
+//if strawberry alert all out of strawberry.
+//prompt for flavor, then output heres your flavor
+//Else alert no such flavor
+//XXConinue while flavor is not = to coffee
+//XXThen write no coffee allowed in school
+// Put in a for loop ask for 3 scoops
+// After each scoop alert say heres your flavor
+//after  loop, alert heres your sunday
+
+
+for (i = 1; i < 4; i++) { 
+    
+
+
+
+		var flavor = prompt("Please enter a flavor such as Chocolate, Vanilla or Strawberry");
+		if(flavor == "chocolate" || flavor == "vanilla"){
+			alert("The flavor you selected is "+flavor);
+			alert("Scoop #"+i+". Your flavor is "+flavor);
+		}
+		else if(flavor == "strawberry"){
+			alert("Sorry we do not have any "+flavor+" ice cream left.");
+			i--;
+			alert("Please select a different flavor");
+		}
+		else{
+			alert("Sorry. There is no such flavor in our store");
+			i--;
+			alert("Please select a different flavor");
+		}
+	
+	
+	
+}
